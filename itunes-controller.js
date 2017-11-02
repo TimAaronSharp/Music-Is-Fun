a2ad29d819a43904cfd82aef47bb7d7026674534
Tim Sharp <timothyaaron.sharp@gmail.com>
2017-11-01 20:40:32 -0600

    first commit

--- itunes-controller.js
+++ itunes-controller.js
@@ -8,7 +8,26 @@ function ItunesController(){
   }
 
   //Start coding here
-  function draw(){
+  function draw(songList){
+    console.log(songList)
+    var template = ''
+    for (var i = 0; i < songList.length; i++) {
+      var song = songList[i];
+      song.albumArt = song.albumArt.replace('60x60bb', '200x200bb')
+      template += `
+      <div class="col-md-5 col-md-offset-3 col-sm-6 col-sm-offset-6 m-tb-1 bc br border">
+      <div class="holder">
+      <img src="${song.albumArt}">
+      <p>Artist: ${song.artist}</p>
+      <p>Title: ${song.title}</p>
+      <p>Collection: ${song.collection}</p>
+      <p>$${song.price}</p>
+      <audio src="${song.preview}" type="audio/mpeg" controls></audio>
+      </div>
+      </div>`
+      
+    }
+    document.getElementById("song-list").innerHTML = template
   }
 
 
@@ -16,3 +35,5 @@ function ItunesController(){
 
   
 }
+
+//style="background-image: url(${song.albumArt}); background-repeat:no-repeat; background-position: center;  <--- maybe try to do the background-image thing later.
